caa4c8f6ec5698c726d99279ec0065fdf2e5b161
Jimmy Kittiyachavalit <jimmy@codetry.com>
2008-07-15 11:19:28 +0700

    Use do_mysql-0.2.4.
    do_mysql-0.9.2 is incompatible with datamaper-0.2.5.

--- lib/wink/core_extensions.rb
+++ lib/wink/core_extensions.rb
@@ -58,6 +58,10 @@ def reloading?
   Sinatra.application.reloading?
 end
 
+# datamapper-0.2.5 is incompatible with do_mysql-0.9.2
+gem 'do_mysql', '=0.2.4'
+require 'do_mysql'
+
 gem 'datamapper', '=0.2.5'
 require 'data_mapper'
 
